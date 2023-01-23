d71165102b0143001b7e92f39457af34fe7ed6ad
Szymon Domagała <75216504+goflyy@users.noreply.github.com>
2023-01-23 12:43:34 +0100

    bttn_Confirm

--- UberBarber/UberBarber/View/User.xaml.cs
+++ UberBarber/UberBarber/View/User.xaml.cs
@@ -23,6 +23,7 @@ namespace UberBarber
         {
             InitializeComponent();
         }
+        //This functionality allows you to drag the window to any place
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.LeftButton == MouseButtonState.Pressed)
@@ -32,12 +33,12 @@ namespace UberBarber
         {
 
         }
-
+        //Minimize the window
         private void btnMinimize_Click(object sender, RoutedEventArgs e)
         {
             WindowState = WindowState.Minimized;
         }
-
+        //Close this window
         private void btnClose_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
