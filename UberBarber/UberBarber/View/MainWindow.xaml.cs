d5367371a4084371710742e9a5b1da091865509f
Szymon Domagała <75216504+goflyy@users.noreply.github.com>
2023-01-27 23:26:58 +0100

    full screen expansion, ability to revert back to standard size

--- UberBarber/UberBarber/View/MainWindow.xaml.cs
+++ UberBarber/UberBarber/View/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using Org.BouncyCastle.Asn1.X509;
 
 namespace UberBarber
 {
@@ -23,11 +26,21 @@ namespace UberBarber
         public MainWindow()
         {
             InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
         }
 
         private void button_user_Click(object sender, RoutedEventArgs e)
         {
             // TODO: users handling
+        }
+        //full screen funcitonality and drag move
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, int wMsg, int wParam, int lParam);
+
+        private void pnlControlBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WindowInteropHelper helper = new WindowInteropHelper(this);
+            SendMessage(helper.Handle, 161, 2, 0);
         }
     }
 }
