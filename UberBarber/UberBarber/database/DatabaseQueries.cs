0e4c619107dee2214c524da59c88165edf6500ad
MSkrzypinski <pulospl1@gmail.com>
2023-02-06 05:03:46 +0100

    fix password validation

--- UberBarber/UberBarber/database/DatabaseQueries.cs
+++ UberBarber/UberBarber/database/DatabaseQueries.cs
@@ -71,7 +71,7 @@ namespace UberBarber.database
         {
             string message = "Something went wrong :(";
             string usernamePattern = "^[a-zA-Z0-9.]{6,}$";
-            string passwordPattern = "^\\S{6,}$";
+            string passwordPattern = "^[a-zA-Z]{6,}$";
             string emailPattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
             // password validation
