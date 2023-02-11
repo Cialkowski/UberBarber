83c8a66dbd4c62d23d3a259c2dfd90a175808bfb
MSkrzypinski <pulospl1@gmail.com>
2023-02-11 21:44:42 +0100

    add barber class

---
+++ UberBarber/UberBarber/Barber/Barber.cs
@@ -0,0 +1,26 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UberBarber.Barber
+{
+    public class Barber
+    {
+        public Barber(MySqlDataReader reader)
+        {
+            Name = (string)reader["name"];
+            Title = (string)reader["title"];
+            PhoneNumber = (string)reader["phone_number"];
+            Age = (int)reader["age"];
+            UserId = (int)reader["user_id"];
+        }
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Age { get; set; }
+        public int UserId { get; set; }
+    }
+}
